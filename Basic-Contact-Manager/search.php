0121e7926c664416382c9f7e5286e6d79c23aae7
Hagar Elbakry <161838415+Hagar-Elbakry@users.noreply.github.com>
2025-03-18 00:12:49 +0200

    Update search.php

--- Basic-Contact-Manager/search.php
+++ Basic-Contact-Manager/search.php
@@ -28,26 +28,7 @@
               <a href="/">Back to From..</a>
         </form>';
         endif;
-
-
-    //     if(isset($res) && $res == ''):
-    //         echo '<h3 style="color: red;">' . "No matched recored for this name<br>" . '</h>';
-    //     elseif(isset($error)):
-    //         echo '<h3 style="color: red;">' . $error . '</h>';
-    //     endif;
-
-    //     if(isset($res) && $res!=''):
-    //         echo $res;
-    //         echo '<a href="/">Back to From..</a>';
-    //         echo "<br>";
-    //         echo '<a href="search.php">Go To Search..</a>'; 
-    //     else:
-    //         echo '<form method="post">
-    //     <input type="text" name="search" placeholder="Search with name.."><br>
-    //      <button type="submit" name="search">Search</button>
-    // </form>';
-    // endif;
-    // ?>
+ ?>
 
 </body>
 </html>
